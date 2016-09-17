0b49407d8decd4d5b3042fa7be7d7d815da7c6be
hgdsraj <raj.axisos@gmail.com>
2016-09-17 16:35:51 -0400

    Working - Added bounciness to ball, paddle integration, oculus integration, character movement integration

---
+++ VR_Ping_Pong/Assets/force.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class force : MonoBehaviour {
+    public float thrust;
+    public Rigidbody rb;
+
+    void Start() 
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * thrust);
+
+    }
+
+    void FixedUpdate() 
+    {
+    }
+}
